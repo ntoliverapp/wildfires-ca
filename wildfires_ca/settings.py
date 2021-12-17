a643465936fc8622b3b138be8694206f934f6b0a
ntoliverapp <n.toliverapplication@gmail.com>
2021-12-17 09:40:52 -0800

    sqlalchemy

--- wildfires_ca/settings.py
+++ wildfires_ca/settings.py
@@ -1,4 +1,6 @@
 import os
-
+import re
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SECRET_KEY = os.environ.get('SECRET_KEY')
