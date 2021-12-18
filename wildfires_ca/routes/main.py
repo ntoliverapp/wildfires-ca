19ed189c120d754a95e7f2f15990342ec0aa330e
ntoliverapp <n.toliverapplication@gmail.com>
2021-12-17 16:06:43 -0800

    updates

--- wildfires_ca/routes/main.py
+++ wildfires_ca/routes/main.py
@@ -59,7 +59,7 @@ def search_result():
 
 @main.route("/database", methods=["POST", "GET"])
 def database():
-    return render_template("database.html", wildfires=Wildfire.display(30))
+    return render_template("database.html", wildfires=Wildfire.display(50))
 
 @main.route("/info", methods=["POST", "GET"])
 def info():
